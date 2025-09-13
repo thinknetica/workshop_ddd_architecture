938ad0b6244c16dae6507a417b67c58cf5307694
Igor Simdyanov <i.simdanov@netology.ru>
2025-09-13 17:50:45 +0300

    Расширяем описание подключения keycloak

--- day3/oauth-keycloak/config/routes.rb
+++ day3/oauth-keycloak/config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root "home#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
